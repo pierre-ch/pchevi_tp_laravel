391ec053302de3f4591c43e30efeeec45f035dec
pierre-ch <96068634+pierre-ch@users.noreply.github.com>
2023-03-06 12:02:13 +0100

    début tp 2

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -85,7 +85,7 @@ class FilmController extends Controller
             ]
         ];
         //appel de la vue listFilms en lui passant la liste des films
-        return response()->view('listFilms', ['films' => $films])
+        return response()->view('backoffice\film\listFilms', ['films' => $films])
         ->header('X-HEADER-ONE','Value 1')
         ->cookie('cookie_one','value1')
         ->header('Content-Type','text/html');
