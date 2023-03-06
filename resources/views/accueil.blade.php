02ba19db261a3816de62c15da8e801dbe0ed64a2
pierre-ch <96068634+pierre-ch@users.noreply.github.com>
2023-03-06 09:53:55 +0100

    routage (fini)

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,7 @@
+<html>
+<title>Accueil</title>
+<body>
+Bonjour {{ $user }}<br/>
+Bienvenue sur notre site de passioné(e)s de ...
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,93 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::any('/', function () {
+//     return 'Index pour toutes les méthodes';
+// });
+// Route::match(['put', 'post' ,'get'], '/', function () {
+//     return 'Index pour les méthodes put,post,get';
+// });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/film/Babylon', function() { return 'Film titre: Babylon';
+// });
+
+// Route::get('/', function () {return view('welcome');});
+// Route::get('/film/Babylon', function() { return "Film1 titre: Babylon";});
+// Route::get('/film/{titre}', function($titre) { return "Film2 titre:{$titre}";});
+
+// Route::get('/film/{titre}/{genre?}', function($titre,$genre = '???') { return "Film3 titre:{$titre} , genre : {$genre}";});
+
+// Route::get('/film/{titre}/{genre?}', function($titre,$genre = '???') { return "Film3 titre:{$titre} , genre : {$genre}";});
+// Route::get('/film/{titre}', function($titre) { return "Film2 titre:{$titre}";});
+
+// Route::get('/film/{titre}/{genre?}', function($titre,$genre = '???') { 
+//     $x = request()->segments();
+//     if(count($x)==3)
+//     {return " Film3 titre:{$titre} , genre : {$genre}";}
+//     else
+//     {return "Film2 titre:{$titre}";}
+// });
+
+// Route::get('/', function () {return view('welcome');});
+// Route::get('/film/{titre}', function($titre) { return "Film titre:{$titre}";})->name('film.affiche');
+
+// Route::get('/film/list', function () { return '<ul>
+//     <li><a href="'.route('film.affiche', ['Babylon']).'">Babylon</a></li>
+//     <li><a href="'.route('film.affiche', ['Avatar 2']).'">Avatar 2</a></li>
+//     </ul>';
+//     })->name('film.list');
+
+// Route::get('/', function () {return view('welcome');});
+// Route::get('/film/{titre}/{genre}/{year}', function ($titre,$genre, $year) { return "Film titre: {$titre},  genre : {$genre}, année parution {$year}";
+// })->where('year','\d{4}')->name('film.affiche');
+// Route::get('/film/list', function () { return '<ul>
+//     <li><a href="'.route('film.affiche', ['Babylon','Comédie','2023']).'">Babylon</a></li>
+//     <li><a href="'.route('film.affiche', ['Avatar 2','Sciences-Fiction','2022']).'">Avatar 2</a></li>
+//     </ul>';
+//     })->name('film.list');
+
+// Route::get('/', function () {return view('welcome');});
+// Route::group([], function () { 
+//     Route::get('/film/list', function () { 
+//         return '<ul><li><a href="'.route('film.affiche', ['titre' => 'Babylon','genre'=>'Comédie','year'=>'2023']).'">Babylon</a></li>
+//                 <li><a href="'.route('film.affiche', ['year'=>'2022','titre' =>'Avatar 2','genre'=>'Sciences-Fiction']).'">Avatar 2</a></li></ul>';
+//     })->name('film.list');
+//     Route::get('/film/{titre}/{genre}/{year}', function ($titre,$genre, $year) { return "Film titre: {$titre},  genre : {$genre}, année parution {$year}";
+//     })->where('year','\d{4}')->name('film.affiche');
+
+// });
+
+// Route::get('/', function () {return view('welcome');});
+// Route::prefix('film')->group(function () { 
+//     Route::get('list', function () { 
+//         return '<ul><li><a href="'.route('film.affiche', ['titre' => 'Babylon','genre'=>'Comédie','year'=>'2023']).'">Babylon</a></li>
+//                 <li><a href="'.route('film.affiche', ['year'=>'2022','titre' =>'Avatar 2','genre'=>'Sciences-Fiction']).'">Avatar 2</a></li></ul>';
+//     })->name('films.list');
+//     Route::get('{titre}/{genre}/{year}', function ($titre,$genre, $year) { return "Film titre: {$titre},  genre : {$genre}, année parution {$year}";
+//     })->where('year','\d{4}')->name('film.affiche');
+// });
+
+// Route::get('/', function () {return view('welcome');});
+// Route::prefix('film')
+//         ->name('film.')
+//         ->middleware('auth')
+//         ->group(function () { 
+//     Route::get('list', function () { 
+//         return '<ul><li><a href="'.route('film.affiche', ['titre' => 'Babylon','genre'=>'Comédie','year'=>'2023']).'">Babylon</a></li>
+//                 <li><a href="'.route('film.affiche', ['year'=>'2022','titre' =>'Avatar 2','genre'=>'Sciences-Fiction']).'">Avatar 2</a></li></ul>';
+//     })->name('list');
+//     Route::get('{titre}/{genre}/{year}', function ($titre,$genre, $year) { return "Film titre: {$titre},  genre : {$genre}, année parution {$year}";
+//     })->where('year','\d{4}')->name('affiche');
+// });
+// Route::get ('/login', function() { return "Authentication required !";
+// })->name('login');
+
+Route::get('/', function () {return view('welcome');});
+Route::get('/film/{titre}', function($titre) { return "Film titre:{$titre}";})->name('film.affiche');
+Route::redirect('/film/{titre}', '/');
+Route::get('/accueil', function () {return view('accueil');});
+Route::get('/accueil', function () {return view('accueil',['user' => 'Elève BTS SIO 2 Carcouet']);});
